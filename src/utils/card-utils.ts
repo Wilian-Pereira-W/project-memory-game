83bfcd6bff178f10db6be217d4f69cc88fb6ae6a
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-13 11:03:35 -0300

    feat: implementado sortArray

--- src/utils/card-utils.ts
+++ src/utils/card-utils.ts
@@ -1,3 +1,7 @@
 export const duplicateArray = <T>(array: T[]): T[] => {
   return array.concat(array);
 };
+
+export const sortArray = <T>(array: T[]): T[] => {
+  return array.sort(() => Math.random() - 0.5);
+};
