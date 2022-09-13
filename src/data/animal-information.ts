3ccd8b3e1a07b096af25b19556e092d383d85109
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-13 10:53:17 -0300

    feat: implementado informações dos animais

---
+++ src/data/animal-information.ts
@@ -0,0 +1,63 @@
+import aguia from '../assets/images/aguia.png';
+import elefante from '../assets/images/elefante.png';
+import galinha from '../assets/images/galinha.png';
+import gato from '../assets/images/gato.png';
+import girafa from '../assets/images/girafa.png';
+import hipopotamo from '../assets/images/hipopotamo.png';
+import pato from '../assets/images/pato.png';
+import peixe from '../assets/images/peixe.png';
+import pinguim from '../assets/images/pinguim.png';
+import raposa from '../assets/images/raposa.png';
+
+export const data = [
+  {
+    id: '1',
+    name: 'Águia',
+    url: aguia,
+  },
+  {
+    id: '2',
+    name: 'Elefante',
+    url: elefante,
+  },
+  {
+    id: '3',
+    name: 'Galinha',
+    url: galinha,
+  },
+  {
+    id: '4',
+    name: 'Gato',
+    url: gato,
+  },
+  {
+    id: '5',
+    name: 'Girafa',
+    url: girafa,
+  },
+  {
+    id: '6',
+    name: 'Hipopótamo',
+    url: hipopotamo,
+  },
+  {
+    id: '7',
+    name: 'Pato',
+    url: pato,
+  },
+  {
+    id: '8',
+    name: 'Peixe',
+    url: peixe,
+  },
+  {
+    id: '9',
+    name: 'Pinguim',
+    url: pinguim,
+  },
+  {
+    id: '10',
+    name: 'Raposa',
+    url: raposa,
+  },
+];
