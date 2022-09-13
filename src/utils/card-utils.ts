519eb81a02fe557c972d12a15e02882be8efeb46
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-13 11:04:40 -0300

    feat: implementado keyGen e regenerateCard

--- src/utils/card-utils.ts
+++ src/utils/card-utils.ts
@@ -1,7 +1,20 @@
+import { CardProps } from '../components/Card';
+
+const keyGen = (): string => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
+};
+
 export const duplicateArray = <T>(array: T[]): T[] => {
   return array.concat(array);
 };
 
 export const sortArray = <T>(array: T[]): T[] => {
   return array.sort(() => Math.random() - 0.5);
+};
+
+export const regenerateCard = (cards: CardProps[]): CardProps[] => {
+  return cards.map((card) => ({ ...card, id: keyGen() }));
 };
